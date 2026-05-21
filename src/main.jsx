02f592b1ce2b2e831ae4c154cda7e7c48daff2e4
Zann-Dot <anaykarn234@gmail.com>
2026-05-21 16:45:50 +0530

    feat: implement preline and daisyUI theme generator workflow with full project scaffolding and dependency setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+// import $ from "jquery";
+// import _ from "lodash";
+// import Dropzone from "dropzone";
+// import noUiSlider from "nouislider";
+// import DataTable from "datatables.net";
+// import { Calendar } from "vanilla-calendar-pro";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// window.$ = $;
+// window.jQuery = $;
+// window._ = _;
+// window.Dropzone = Dropzone;
+// window.noUiSlider = noUiSlider;
+// window.DataTable = DataTable;
+// window.VanillaCalendarPro = Calendar;
+
+import("preline").then(() => {
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  );
+});
